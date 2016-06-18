c1ff4434c34dbb3784b2572651ab8d9eaad6af7a
ro-double-b <robb.marsee@gmail.com>
2016-06-18 12:02:17 -0700

    Set background image and images for player and enemies.

--- client/watchout.js
+++ client/watchout.js
@@ -1,15 +1,10 @@
-// start slingin' some d3 here.
-// dataset that includes the enimies
-  // will be an array of objs
-  // each obj will be one eniemi
-
-var dataset = [1, {'cy': 500, 'cx': 500}, {'cy': 500, 'cx': 500}, {'cy': 500, 'cx': 500}];
-var playerSet = [{'x': 500, 'y': 250}];
+var dataset = [1, 2, 3, 4, 5];
+var playerSet = [{'x': 500, 'y': 300}];
 var drag = d3.behavior.drag()
   .on('drag', function() {
-    player.attr('cx', d3.event.x)
-    .attr('cy', d3.event.y);
-    playerSet[0].x = d3.event.x;
+    player.attr('x', d3.event.x)
+    .attr('y', d3.event.y);
+    playerSet[0].x = d3.event.x + 100;
     playerSet[0].y = d3.event.y;
   });
 
@@ -17,9 +12,7 @@ var currentScore = 0;
 var highScore = currentScore;
 var collision = 0;
 
-
 var scoreNode = d3.select('body').select('div.scoreboard').select('div.current').select('span');
-
 
 var checkCollision = function() {
   currentScore++;
@@ -27,67 +20,61 @@ var checkCollision = function() {
     highScore = currentScore;
   }
 
-  //iterate over array of enemies attributes
-  //check to see if any toutch
-  //if they do its a collsion
-
   for (var i = 1; i < dataset.length; i++) {
-    if (Math.abs(playerSet[0].y - enemies[0][i].cy.animVal.value) <= 50 && Math.abs(playerSet[0].x - enemies[0][i].cx.animVal.value) <= 50) {
-      currentScore = 0;
-      collision++;  
-  }
 
+    var collisionFlag = false
+    // checks if a collision is true; sets flag to true
+    if (!collisionFlag && Math.abs(player[0][0].y.animVal.value - enemies[0][i].y.animVal.value) <= 50 && Math.abs(player[0][0].x.animVal.value - enemies[0][i].x.animVal.value) <= 50) {
+      collisionFlag = true;
+    }
+    if (collisionFlag && Math.abs(player[0][0].y.animVal.value - enemies[0][i].y.animVal.value) < 50 && Math.abs(player[0][0].x.animVal.value - enemies[0][i].x.animVal.value) < 50) {
+      currentScore = 0;
+      collision++;
+      collisionFlag = false;
+    }
   }
   
   d3.select('div.current').select('span').text(currentScore);
   d3.select('div.highscore').select('span').text(highScore);
   d3.select('div.collisions').select('span').text(collision);
-  // hello.data([2, 3, 4]).text(currentScore);
-
-  // d3.select('body').selectAll('span').data(currentScore).text(function(d) { return d; });
 };
 
-
-
 var board = d3.select('.board').append('svg')
-            .attr('width', 1000)
-            .attr('height', 500);
+  .attr('width', 1000)
+  .attr('height', 667)
+  .style('background', 'url(BGspace.jpeg)')
+  .style('background-size', 'contain');
 
-
-var player = board.selectAll('circle')
+var player = board.selectAll('image')
   .data(playerSet)
   .enter()
-  .append('circle')
-  .attr('r', 25)
-  .attr('fill', 'red')
+  .append('image')
+  .attr('width', 75)
+  .attr('height', 70)
+  .attr('xlink:href', 'falcon.gif')
   .attr('class', 'player')
-  .attr('cx', function(d) {
-    return d.x;
-  })
-  .attr('cy', function(d) {
-    return d.y;
-  })
+  .attr('x', function(d) { return d.x; })
+  .attr('y', function(d) { return d.y; })
   .call(drag);
 
 
-
-var enemies = board.selectAll('circle')
+var enemies = board.selectAll('image')
   .data(dataset)
   .enter()
-  .append('circle')
-  .attr('cx', function(d) { return Math.random() * 1000; })
-  .attr('cy', function(d) { return Math.random() * 500; })
-  .attr('r', 25)
-  .attr('class', 'enemies');
-// dataset of # enemies
+  .append('image')
+  .attr('width', 50)
+  .attr('height', 50)
+  .attr('xlink:href', 'asteroid.png')
+  .attr('class', 'enemies')
+  .attr('x', function(d) { return Math.random() * 1000; })
+  .attr('y', function(d) { return Math.random() * 667; });
+
 var move = function() {
 
   enemies.transition()
   .delay(750)
-  .attr('cy', function(d, i) { return Math.random() * 500; })
-  .attr('cx', function(d, i) { return Math.random() * 1000; });
-
-
+  .attr('y', function(d, i) { return Math.random() * 667; })
+  .attr('x', function(d, i) { return Math.random() * 1000; });
 };
 
 move();
